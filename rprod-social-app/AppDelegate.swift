9aec7f50c9d1e66402c059069833dedd0f7fea21
Rasmus Josefsson <rasmusjosefsson@Rasmuss-MacBook-Pro.local>
2017-01-02 01:01:35 +0100

    facebook auth with firebase added

--- rprod-social-app/AppDelegate.swift
+++ rprod-social-app/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+       
+        // Firebase Setup
         FIRApp.configure()
+        
+        // Facebook Login Setup
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
 
@@ -42,7 +49,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
 
+    
+    /*
+     
+     BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+     openURL:url
+     sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+     annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+     ];
+     
+     */
 }
 
--- rprod-social-app/SignInVC.swift
+++ rprod-social-app/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -15,8 +18,37 @@ class SignInVC: UIViewController {
        
     }
 
+    
+    func firebaseAuth(_ fireCredential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: fireCredential, completion: { (fireUser, error) in
+            if error != nil {
+                print("RASMUS: Unable to autenticate with Firebase: \(error)")
+            } else {
+                print("Rasmus: User Successfully autenticated with Firebase")
+            }
+        })
+    }
+    
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+           
+            if error != nil {
+                print("RASMUS: Unable to autenticate with facebook \(error)")
+            } else if result?.isCancelled == true {
+                print("RASMUS: User cancelled Facebook autentication")
+            } else {
+                print("Rasmus: User Successfully autenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+            
+        }
+    }
 
+    
 
 }
 
